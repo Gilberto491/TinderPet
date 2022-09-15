6b0d5d0fcf7b04cd1d122fdfcd41e709f7b7c63d
gilberto.junior <gilberto.junior@dbccompany.com.br>
2022-09-15 14:58:47 -0300

    carousel finished and page not found

--- app/src/pages/notFound/index.tsx
+++ app/src/pages/notFound/index.tsx
@@ -1,25 +1,24 @@
 import NotFound from "../../img/notFoundv2.jpg";
 import Button from "../../components/button";
 import "./notFound.scss";
-import { AssertionError } from "assert";
 
-export default function login() {
+export default function notFound() {
     return (
-        <div>
-            <div className="container text-center">
-            <div className="row">
-                <div className="col">
-                    <img className="image-page" src={NotFound}></img>
-                </div>
-                <div className="col">
-                <h2 className="margin-page">Página não encontrada <Button/></h2>  
-                </div>
-                <div className="col">
-                </div>
-                <div className="col">
+        <aside>
+            <div className="body-notfound">
+            <div id="notfound">
+                <div className="notfound">
+                    <div className="notfound-404">
+                        <img className="img-notfound" src={NotFound}/>
+                    </div>
+                    <h3>404 - Página não encontrada</h3>
+                    <p>A página que você está procurando está temporariamente indisponível.</p>
+                    <Button/>
                 </div>
             </div>
-            </div>
+            
         </div>
+       
+        </aside>
     );
 }
